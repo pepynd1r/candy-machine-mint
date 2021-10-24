e8028ac670b6aa6d5a130031c7ec7d670ab0f7ba
pepo <pepynd1r@gmail.com>
2021-10-24 17:26:14 +0300

    Updates

--- static/js/main.js
+++ static/js/main.js
@@ -10,7 +10,10 @@ $( document ).ready(function() {
          var walletBalance = $('#root main p:nth-child(2)').text();
          walletBalance = walletBalance.replace("Balance:", "");
          var nftsAvailable = $('#root main p:nth-child(3)').text();
+         nftsAvailable = nftsAvailable.replace("Total Available:", "");
          var nftsRemaining = $('#root main p:nth-child(5)').text();
+         nftsRemaining = nftsRemaining.replace("Remaining:", "");
+
          $('.wallet-name').html( walletName );
          $('.wallet-balance').html( walletBalance );
          $('.nft-available').html( nftsAvailable );
