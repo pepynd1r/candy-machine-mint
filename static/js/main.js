fa6b3c1d3f3cad58fd5bc4b26872cb311a1f4b5e
pepo <pepynd1r@gmail.com>
2021-10-24 16:52:53 +0300

    Updates

--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,5 @@
-const { url } = require("inspector");
-
 $( document ).ready(function() {
-    itemsAvailable = undefined;
-    function waitForElement(){
-        if(typeof itemsAvailable !== "undefined"){
-            //variable exists, do what you want
-            console.log('asdas');
-            console.log(itemsAvailable);
-        }
-        else{
-            setTimeout(waitForElement, 250);
-            console.log('Noasdas');
-            console.log(itemsAvailable);
-        }
-    }
-    
-    waitForElement();
+
 
 });
 
