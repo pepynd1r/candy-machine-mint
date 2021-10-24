2df659c11e2a1d7ea6b4fdf9a6f3d55201c1568c
pepo <pepynd1r@gmail.com>
2021-10-24 17:21:28 +0300

    Updates

--- static/js/main.js
+++ static/js/main.js
@@ -8,6 +8,7 @@ $( document ).ready(function() {
     $(document).ready( setInterval(function() { 
          var walletName = $('#root main p:first-child').text();
          var walletBalance = $('#root main p:nth-child(2)').text();
+         walletBalance = walletBalance.replace("Balance:", "");
          var nftsAvailable = $('#root main p:nth-child(3)').text();
          var nftsRemaining = $('#root main p:nth-child(5)').text();
          $('.wallet-name').html( walletName );
