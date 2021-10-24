66584e3d370bcc61af7fd60b8c10a579a5e1c7d3
pepo <pepynd1r@gmail.com>
2021-10-24 17:56:51 +0300

    Updates

--- static/js/main.js
+++ static/js/main.js
@@ -19,4 +19,8 @@ $( document ).ready(function() {
          $('.wallet-balance').html( walletBalance );
          $('.nft-available').html( nftsAvailable );
          $('.nfts-remaining').html( nftsRemaining );
+
+         if (walletName !== '') {
+            $('.solana-wallet').css('display','list-item');
+         }
     }, 1000)); 
