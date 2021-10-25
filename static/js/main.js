b00dc45f62b5aa842abdd37ec7fd5aec6fbe564e
pepo <pepynd1r@gmail.com>
2021-10-25 19:21:38 +0300

    Updates

--- static/js/main.js
+++ static/js/main.js
@@ -24,7 +24,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
     $('#root').css("display","block");
   }
 }, 1000);
