6b13e49a9b75b0755dfeb2a1e502603e5169f58c
pepo <pepynd1r@gmail.com>
2021-10-25 19:16:03 +0300

    Updates

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 25, 2021 17:15:00").getTime();
+var countDownDate = new Date("Oct 25, 2021 19:20:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
