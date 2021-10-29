012a0e1d66eb2b5034420cc3d378ee9d3dfc9f10
pepo <pepynd1r@gmail.com>
2021-10-29 19:52:40 +0300

    Updates

--- static/js/main.js
+++ static/js/main.js
@@ -30,7 +30,27 @@ var x = setInterval(function() {
 
 });
 
+    // Check if the page has loaded completely                                         
+    $(document).ready( setInterval(function() { 
+         var walletName = $('#root main p:first-child').text();
+         walletName = walletName.replace("Wallet", "");
+         var walletBalance = $('#root main p:nth-child(2)').text();
+         walletBalance = walletBalance.replace("Balance:", "");
+         var nftsAvailable = $('#root main p:nth-child(3)').text();
+         nftsAvailable = nftsAvailable.replace("Total Available:", "");
+         var nftsRemaining = $('#root main p:nth-child(5)').text();
+         nftsRemaining = nftsRemaining.replace("Remaining:", "");
 
+         $('.wallet-name').html( walletName );
+         $('.wallet-balance').html( walletBalance );
+         $('.nft-available').html( nftsAvailable );
+         $('.nfts-remaining').html( nftsRemaining );
+
+         if (walletName !== '') {
+            $('.solana-wallet').css('display','list-item');
+            $('.globes-info').css('display','block');
+         }
+    }, 1000)); 
 
 
 
